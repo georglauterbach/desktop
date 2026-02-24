f7b0b65d5434aa77b61c01c45f0f44cb3da2934d
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2026-02-24 21:56:33 +0100

    chore: convert startup applications to systemd services
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

--- config/.config/sway/init.sh
+++ config/.config/sway/init.sh
@@ -1,45 +1,30 @@
 #! /usr/bin/env bash
 
-# cSpell: ignore SWAYSOCK shikane swaync waybar
-
-# Some variables need to be exported to systemd
-# and/or dbus activation environments so tools
-# that rely on them work properly.
-#
-# Applications that are started as systemd (user)
-# services or that rely on D-Bus, or those that
-# trigger D-Bus, MUST be started in
-#
-# ${HOME}/.config/sway/user/init.sh
-#
-# to ensure they are started AFTER we imported
-# the correct environment variables to systemd
-# and D-Bus.
-#
-# ref: https://github.com/swaywm/sway/wiki#systemd-and-dbus-activation-environments
-# ref: https://github.com/swaywm/sway/issues/5841
+# cSpell: ignore SWAYSOCK
 
 set +eE -u -o pipefail
 shopt -s inherit_errexit
 
-systemctl --user import-environment          DISPLAY WAYLAND_DISPLAY SWAYSOCK I3SOCK
-dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK I3SOCK
-
-for SERVICE in xdg-desktop-portal{,-wlr,-gtk}.service; do
+function systemd_start_user_service() {
+  local SERVICE=${1:?Service name required}.service
   if systemctl --user is-failed "${SERVICE}" &>/dev/null; then
     systemctl --user restart "${SERVICE}"
   elif ! systemctl --user is-active "${SERVICE}" &>/dev/null; then
     systemctl --user start "${SERVICE}"
   fi
-  systemctl --user status "${SERVICE}" &>>/tmp/service_status
+}
+
+# Some variables need to be exported to systemd and/or dbus activation
+# environments so tools that rely on them work properly.
+readonly ENVIRONMENT=(DISPLAY WAYLAND_DISPLAY SWAYSOCK I3SOCK)
+systemctl --user import-environment          "${ENVIRONMENT[@]}"
+dbus-update-activation-environment --systemd "${ENVIRONMENT[@]}" || :
+
+readonly SERVICES=(xdg-desktop-portal{,-wlr,-gtk} shikane ianny waybar swaync)
+for SERVICE in "${SERVICES[@]}"; do
+  systemd_start_user_service "${SERVICE}"
 done
 unset SERVICE
 
-bash -c 'exec shikane           >/tmp/.sway.shikane.log 2>/tmp/.sway.shikane.err.log' &
-bash -c 'exec waybar -l warning >/tmp/.sway.waybar.log  2>/tmp/.sway.waybar.err.log'  &
-bash -c 'exec swaync            >/tmp/.sway.swaync.log  2>/tmp/.sway.swaync.err.log'  &
-
-disown -a
-
 # shellcheck source=/dev/null
-[[ ! -e ${HOME}/.config/sway/user/init.sh ]] || source "${HOME}/.config/sway/user/init.sh"
+[[ ! -s ${HOME}/.config/sway/user/init.sh ]] || source "${HOME}/.config/sway/user/init.sh"
--- config/.config/sway/user/init.sh
+++ config/.config/sway/user/init.sh
@@ -1,8 +1,3 @@
 #! /usr/bin/env bash
 
-bash -c 'exec ianny >/tmp/.sway.ianny.log 2>/tmp/.sway.ianny.err.log' &
-
-readonly LATITUDE='51.0' LONGITUDE='13.7'
-bash -c "exec wlsunset -t 5500 -T 8000 -l ${LATITUDE} -L ${LONGITUDE} >/tmp/.sway.wlsunset.log 2>/tmp/.sway.wlsunset.err.log" &
-
-disown -a
+systemd_start_user_service wlsunset
